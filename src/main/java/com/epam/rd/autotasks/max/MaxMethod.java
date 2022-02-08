b5cbfeef887d78b8ba24f5e1eeeb922e11f224a2
AnnaTsukiyo <annett.sav4enkko@gmail.com>
2022-02-08 21:19:11 +0200

    done

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,9 +1,18 @@
 package com.epam.rd.autotasks.max;
 
+import java.util.Arrays;
 import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 public class MaxMethod {
     public static OptionalInt max(int[] values) {
-        throw new UnsupportedOperationException();
+        if (values == null || values.length == 0) {
+            OptionalInt empty = OptionalInt.empty();
+            return empty;
+        }
+        IntStream intStream = Arrays.stream(values);
+        OptionalInt optionalInt = intStream.max();
+        int maxAsInt = optionalInt.getAsInt();
+        return optionalInt;
     }
 }
